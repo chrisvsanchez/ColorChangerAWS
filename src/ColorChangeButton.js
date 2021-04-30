f2f5c89f90aaa199994668606e1b953f1d772529
chrisvsanchez <chrisvsanchez@gmail.com>
2021-04-30 13:35:30 -0400

    updated props in colorchangebutton to update state in App.js

--- src/ColorChangeButton.js
+++ src/ColorChangeButton.js
@@ -1,6 +1,10 @@
 import React from "react";
 
-function ColorChangeButton({ color }) {
-  return <button className={color}>{color}</button>;
+function ColorChangeButton({ color, setColor }) {
+  return (
+    <button className={color} onClick={() => setColor(`${color}`)}>
+      {color}
+    </button>
+  );
 }
 export default ColorChangeButton;
