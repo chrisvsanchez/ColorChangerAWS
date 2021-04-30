18ae737e37f7d1c4add081a014527af949c02b22
chrisvsanchez <chrisvsanchez@gmail.com>
2021-04-29 23:48:45 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="react-root">
+      <div className="centered">
+        <h1> Color Picker</h1>
+        <button className="red">Red</button>
+        <button className="blue">Blue</button>
+        <button className="yellow">Yellow</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
